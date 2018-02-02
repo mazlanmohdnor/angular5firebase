f0a33898fa7f7aa786ebe1dba91f2a873ccb060a
mazlanmohdnor <lan.psis@gmail.com>
2018-02-02 17:06:31 +0800

    crud angular firebase

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,21 +11,40 @@ export class HomeComponent implements OnInit {
   wishes = [];
   wishcount = 0;
 
-  constructor(public afd: AngularFireDatabase) {
+  constructor(public afd: AngularFireDatabase) {}
+
+  ngOnInit() {
+    this.afd.database.ref("wishlist").on("value", snapshot => {
+      this.wishes = this.snapshotToArray(snapshot);
+      console.log(this.wishes);
+    });
   }
 
-  ngOnInit() {}
+  snapshotToArray(snapshot) {
+    const returnArr = [];
+
+    snapshot.forEach((childSnapshot) => {
+      const item = childSnapshot.val();
+      const itemkey = childSnapshot.key;
+      returnArr.push({
+        key: itemkey,
+        item: item
+      });
+    });
+
+    return returnArr;
+  }
 
   addWish() {
     this.wishes.push(this.wishtext);
-     this.afd.database
-       .ref("wishlist")
-       .push({ wishlist: this.wishtext });
+    this.afd.database.ref("wishlist").push(this.wishtext);
     this.wishtext = "";
     this.wishcount = this.wishes.length;
   }
   remove(i) {
-    this.wishes.splice(i, 1);
+    // this.wishes.splice(i, 1);
+    console.log(i);
+    this.afd.database.ref(`wishlist/${i}`).remove();
     this.wishcount = this.wishes.length;
   }
 }
